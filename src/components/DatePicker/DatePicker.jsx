da43c3a91446076d81dc9531d1f2bc1a6126b588
Ivan Samozhenov <i_dont_known@mail.ru>
2024-02-21 22:21:21 +0300

    Finish layout of datapicker component

--- src/components/DatePicker/DatePicker.jsx
+++ src/components/DatePicker/DatePicker.jsx
@@ -1,12 +1,23 @@
 import ArrowLeft from '../../icons/ArrowLeft';
+import ArrowRight from '../../icons/ArrowRight';
+import CalendarIcon from '../../icons/CalendarIcon';
 import './style.css';
 
 const DatePicker = () => {
   const currentDate = new Date();
   console.log(currentDate);
   return (
-    <div className="wrapper">
-      <ArrowLeft />
+    <div className="datepicker-wrapper">
+      <button aria-label="Предыдущий день" className="date-control" onClick={() => console.log('lalal')} type="button">
+        <ArrowLeft />
+      </button>
+      <div className="date-container">
+        <CalendarIcon />
+        <span className="date-text">24 марта</span>
+      </div>
+      <button aria-label="Следующий день" className="date-control" onClick={() => console.log('lalal')} type="button">
+        <ArrowRight />
+      </button>
     </div>
   );
 };
