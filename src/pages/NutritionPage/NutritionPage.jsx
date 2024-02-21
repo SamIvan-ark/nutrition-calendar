eb9e853deacbd56cc09857a719b5a7a3d21b1df3
Ivan Samozhenov <i_dont_known@mail.ru>
2024-02-21 22:20:33 +0300

    Add general style file, update NutritionPage

---
+++ src/pages/NutritionPage/NutritionPage.jsx
@@ -0,0 +1,13 @@
+import DatePicker from '../../components/DatePicker';
+import './style.css';
+
+const NutritionPage = () => {
+  console.log('lalajopa');
+  return (
+    <div className="wrapper">
+      <DatePicker />
+    </div>
+  );
+};
+
+export default NutritionPage;
