b036e83c4112875436fa40e1db90b374ac0c976c
Ivan Samozhenov <i_dont_known@mail.ru>
2024-02-22 10:52:41 +0300

    Add recognition of current date, add navigation by dates

--- src/components/DatePicker/DatePicker.jsx
+++ src/components/DatePicker/DatePicker.jsx
@@ -1,21 +1,48 @@
+import classnames from 'classnames';
+import { useState } from 'react';
+
 import ArrowLeft from '../../icons/ArrowLeft';
 import ArrowRight from '../../icons/ArrowRight';
 import CalendarIcon from '../../icons/CalendarIcon';
+import {
+  getCurrentDayWithoutHours, isCurrentDay, minusDay, plusDay,
+} from '../../utils/dates';
 import './style.css';
 
 const DatePicker = () => {
-  const currentDate = new Date();
-  console.log(currentDate);
+  const [date, updateDate] = useState(getCurrentDayWithoutHours());
+  const pickedCurrentDay = isCurrentDay(date);
+  const dateFormatter = new Intl.DateTimeFormat('ru-RU', {
+    day: 'numeric',
+    month: 'long',
+  });
+
   return (
     <div className="datepicker-wrapper">
-      <button aria-label="Предыдущий день" className="date-control" onClick={() => console.log('lalal')} type="button">
+      <button
+        aria-label="Предыдущий день"
+        className="date-control"
+        onClick={() => updateDate(minusDay(date))}
+        type="button"
+      >
         <ArrowLeft />
       </button>
       <div className="date-container">
         <CalendarIcon />
-        <span className="date-text">24 марта</span>
+        <span className="date-text">
+          {dateFormatter.format(date)}
+        </span>
       </div>
-      <button aria-label="Следующий день" className="date-control" onClick={() => console.log('lalal')} type="button">
+      <button
+        aria-label="Следующий день"
+        className={classnames(
+          'date-control',
+          { 'button-disabled': pickedCurrentDay },
+        )}
+        disabled={pickedCurrentDay}
+        onClick={() => updateDate(plusDay(date))}
+        type="button"
+      >
         <ArrowRight />
       </button>
     </div>
